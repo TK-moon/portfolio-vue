5a9d8acc781633a3096862e4f6250304ef5cff90
TK-moon <tkmoon.dev@gmail.com>
2021-09-20 23:46:00 +0900

    :rotating_light: ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,32 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "@typescript-eslint/no-var-requires": 0,
-    // allowIndentationTabs: true,
-    "prettier/prettier": [
-      "error",
-      {
-        semi: false,
-        useTabs: true,
-        trailingComma: "all",
-        bracketSpacing: true,
-        arrowParens: "avoid",
-        jsxBracketSameLine: false,
-      },
-    ],
-  },
-};
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		"plugin:vue/vue3-essential",
+		"eslint:recommended",
+		"@vue/typescript/recommended",
+		"@vue/prettier",
+		"@vue/prettier/@typescript-eslint",
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"@typescript-eslint/no-var-requires": 0,
+		"prettier/prettier": [
+			"error",
+			{
+				semi: false,
+				useTabs: true,
+				trailingComma: "all",
+				bracketSpacing: true,
+				arrowParens: "avoid",
+				jsxBracketSameLine: false,
+			},
+		],
+	},
+}
