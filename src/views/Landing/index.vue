9c2f594957bda112507fc76c5f72bb5d237ba2c9
TK-moon <tkmoon.dev@gmail.com>
2021-09-20 22:51:23 +0900

    :hammer: 랜딩 구조 변경

---
+++ src/views/Landing/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <Header />
+  <main>
+    <Main id="main" />
+    <section id="about">About</section>
+    <section id="projects">Projects</section>
+    <section id="contacts">Contact</section>
+  </main>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Main from "./main.vue";
+
+export default {
+  components: {
+    Header,
+    Main,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  section {
+    height: 100vh;
+    border-bottom: 1px solid #000;
+  }
+}
+</style>
---
+++ src/views/Landing/main.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+    <h1 id="headLine" data-aos="fade-up" data-aos-duration="1000">
+      안녕하세요,
+      <br /><span
+        id="name"
+        class="text-bolder"
+        data-aos="zoom-in-up"
+        data-aos-delay="500"
+        >문태경</span
+      >입니다.
+    </h1>
+    <p
+      class="mt-3"
+      data-aos="fade-up"
+      data-aos-duration="1000"
+      data-aos-delay="500"
+    >
+      Web Developer / Frontend / Backend
+    </p>
+    <p
+      class="text-secondary text-300 mt-1"
+      data-aos="fade-up"
+      data-aos-duration="1000"
+      data-aos-delay="1000"
+    >
+      #반응형 #포트폴리오
+    </p>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
