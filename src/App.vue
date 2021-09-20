cdabeee736ef5b4a17827936b526feb2deeb278f
TK-moon <tkmoon.dev@gmail.com>
2021-09-20 17:05:51 +0900

    :sparkles: 헤더/네비게이션 바 PC

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div id="nav">
+  <Header></Header>
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <router-view /> -->
 </template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Header: Header,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+  <header>
+    <nav>
+      <a id="logo" href="main">TK</a>
+      <ol>
+        <li><a href="about">About</a></li>
+        <li><a href="projects">Projects</a></li>
+        <li><a href="contacts">Contacts</a></li>
+      </ol>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+header {
+  position: fixed;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  a {
+    text-decoration: none;
+    color: var(--color-primary-text);
+  }
+}
+#logo {
+  font-size: 32px;
+  font-weight: 900;
+}
+nav {
+  width: 100%;
+  max-width: 1200px;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  ol {
+    display: grid;
+    margin: 0;
+    padding: 0;
+    align-items: center;
+    grid-auto-flow: column;
+    list-style: none;
+    gap: 30px;
+    li {
+      position: relative;
+      &:before {
+        content: "";
+        position: absolute;
+        bottom: -5px;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 3px;
+        background-color: #000;
+        transition: transform ease-in-out 100ms;
+        transform: scaleX(0);
+      }
+      &:hover:before {
+        transform: scaleX(1);
+      }
+    }
+  }
+}
+</style>
