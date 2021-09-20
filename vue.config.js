f932a2e36a9670f376790d4a8c6f8a8134864921
TK-moon <tkmoon.dev@gmail.com>
2021-09-20 17:29:20 +0900

    :hammer: Vue 설정 변경

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  configureWebpack: {
+    devtool: "source-map",
+    entry: "./src/main.ts",
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "src/"),
+      },
+    },
+  },
+  css: {
+    sourceMap: true,
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "@/styles/mixins.scss";
+        `,
+      },
+    },
+  },
+  publicPath: "",
+  productionSourceMap: false,
+};
