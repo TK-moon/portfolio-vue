bdc007b2e56d98bec0798bdaff2edabbbee56dad
TK-moon <tkmoon.dev@gmail.com>
2021-09-20 23:47:16 +0900

    :sparkles: 메인페이지 마이그레이션

--- src/views/Landing/main.vue
+++ src/views/Landing/main.vue
@@ -1,34 +1,12 @@
 <template>
 	<section>
-		<h1 id="headLine" data-aos="fade-up" data-aos-duration="1000">
+		<h1 id="headLine">
 			안녕하세요,
 			<br />
-			<span
-				id="name"
-				class="text-bolder"
-				data-aos="zoom-in-up"
-				data-aos-delay="500"
-			>
-				문태경
-			</span>
-			입니다.
+			<span id="name" class="text-bolder">문태경</span>입니다.
 		</h1>
-		<p
-			class="mt-3"
-			data-aos="fade-up"
-			data-aos-duration="1000"
-			data-aos-delay="500"
-		>
-			Web Developer / Frontend / Backend
-		</p>
-		<p
-			class="text-secondary text-300 mt-1"
-			data-aos="fade-up"
-			data-aos-duration="1000"
-			data-aos-delay="1000"
-		>
-			#반응형 #포트폴리오
-		</p>
+		<p id="keyword">Web Developer / Frontend / Backend</p>
+		<p id="hashtags">#반응형 #포트폴리오</p>
 	</section>
 </template>
 
@@ -42,5 +20,36 @@ export default {}
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
+}
+#headLine {
+	font-size: 4vw;
+	letter-spacing: 2px;
+}
+#name {
+	font-size: 5vw;
+	font-weight: 800;
+}
+#keyword {
+	margin-top: 3em;
+}
+#hashtags {
+	margin-top: 1em;
+	font-weight: 300;
+}
+@include tablet {
+	#headLine {
+		font-size: 8vw;
+	}
+	#name {
+		font-size: 9vw;
+	}
+}
+@include mobile {
+	#headLine {
+		font-size: 12vw;
+	}
+	#name {
+		font-size: 13vw;
+	}
 }
 </style>
