29ff666a0da01452f1f5c1a831d509168d81609c
TK-moon <tkmoon.dev@gmail.com>
2021-12-18 22:27:50 +0900

    :construction: main page title section scroll interaction test

--- src/views/Landing/main.vue
+++ src/views/Landing/main.vue
@@ -13,6 +13,7 @@
 			size="lg"
 			@click="modalVisible = true"
 		/>
+		<p id="trigger" :style="centerStyle">TRIGGER</p>
 		<Modal v-model="modalVisible">
 			<h1>테마 설정</h1>
 			<br />
@@ -40,7 +41,8 @@
 				</ToggleRadio>
 			</div>
 		</Modal>
-		{{ scrollY }}
+		<p>{{ scrollY }}</p>
+		<p>{{ centerScrollY }}</p>
 	</section>
 </template>
 
@@ -73,14 +75,20 @@ export default defineComponent({
 	},
 	setup() {
 		const { colorTheme, setColorTheme } = getColorThemeHandler()
-		const { scrollY } = useScrollY()
+		const { scrollY, centerScrollY } = useScrollY(10)
 		const sectionRef = ref<HTMLElement>()
+
+		const centerStyle = computed(
+			() => `position: absolute; top: ${centerScrollY.value}px`,
+		)
 
 		return {
 			colorTheme,
 			setColorTheme,
 			scrollY,
 			sectionRef,
+			centerScrollY,
+			centerStyle,
 		}
 	},
 	data() {
@@ -89,21 +97,31 @@ export default defineComponent({
 			colorThemeRadio: this.colorTheme,
 		}
 	},
+	computed: {},
 	watch: {
 		colorThemeRadio(nv) {
 			this.setColorTheme({ colorTheme: nv })
 		},
 		scrollY(nv) {
 			if (!this.sectionRef) return
-
-			const scrollHeight = this.sectionRef.offsetHeight / 3
-			const section = this.sectionRef.children[0] as HTMLLIElement
-			const percentage = Math.ceil((nv / scrollHeight) * 100)
-			if (nv > scrollHeight) {
-				section.style.opacity = "0"
-				return
+			let RAFTimeout = null
+			if (RAFTimeout) {
+				cancelAnimationFrame(RAFTimeout)
+			} else {
+				const sectionRef = this.sectionRef
+				requestAnimationFrame(() => {
+					const scrollHeight = sectionRef.offsetHeight / 4
+					const section = sectionRef.children[0] as HTMLLIElement
+					const percentage = (nv / scrollHeight) * 100
+					if (nv > scrollHeight) {
+						section.style.opacity = "0"
+						return
+					}
+					section.style.opacity = (1 - percentage / 100).toString()
+					// section.style.transform = `translateY(${percentage}%)`
+					section.style.transform = `matrix(1, 0, 0, 1, 0, ${percentage * -1})`
+				})
 			}
-			section.style.opacity = (1 - percentage / 100).toString()
 		},
 	},
 	created() {
@@ -124,11 +142,13 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 #main {
+	position: relative;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	justify-content: center;
+	/* justify-content: center; */
 	color: var(--color-primary-text);
+	height: 200vh;
 
 	animation-name: fadeIn, slideUp;
 	animation-duration: 500ms;
@@ -139,17 +159,24 @@ export default defineComponent({
 	font-size: 4vw;
 	letter-spacing: 2px;
 	transition: opacity 100ms linear;
+	transition: opacity 100ms linear, transform 100ms linear;
+	will-change: opacity, transform;
+	position: sticky;
+	top: 40vh;
 }
 #name {
 	font-size: 5vw;
 	font-weight: 800;
 }
 #keyword {
-	margin-top: 3em;
+	/* position: sticky;
+	top: 80vh; */
 }
 #hashtags {
-	margin-top: 1em;
-	font-weight: 300;
+	/* margin-top: 1em; */
+	/* font-weight: 300;
+	position: sticky;
+	top: 85vh; */
 }
 @include tablet {
 	#headLine {
