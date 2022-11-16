4f333ad3aa326f68e64cc907a151503fe0724bd1
TK-moon <tkmoon.dev@gmail.com>
2022-11-16 23:52:09 +0900

    feature: career section 제작 (#6)

--- src/views/pages/Landing/About.vue
+++ src/views/pages/Landing/About.vue
@@ -2,26 +2,28 @@
   <AnimationSection
     disable_multiple_animation
     :animation="[
-      { opacity: 0, translate: '0 0' },
-      { opacity: 1, translate: '0 -50' },
-      { opacity: 0, translate: '0 -100' },
+      { opacity: 0, translate: '0 50' },
+      { opacity: 1, translate: '0 0' },
+      { opacity: 0, translate: '0 -50' },
     ]"
     id="about"
   >
-    <p>안녕하세요,</p>
-    <p>프론트엔드 개발을 하고 있는 문태경입니다.</p>
-    <br />
-    <p>어제보다 발전하기 위해 링크드인 게시글을 스크랩하고</p>
-    <p>Korean FE Article 메일을 구독하여 읽습니다.</p>
-    <br />
-    <p>벌써 5년차가 되었음에도 매일 새로운 것을 배우며,</p>
-    <p>새로운 기술이 나오면 여전히 신기해합니다.</p>
-    <br />
-    <p>안써봤던 기술을 사용하는건 여전히 무섭지만</p>
-    <p>아직까지도 새로운 내용을 배워 업무에 적용하는게 재밌습니다.</p>
-    <br />
-    <p>5년이나 했는데도 질리지 않는걸 보면</p>
-    <p>직업을 잘 고른것 같아요.</p>
+    <div>
+      <p>안녕하세요,</p>
+      <p>프론트엔드 개발을 하고 있는 문태경입니다.</p>
+      <br />
+      <p>어제보다 발전하기 위해 링크드인 게시글을 스크랩하고</p>
+      <p>Korean FE Article 메일을 구독하여 읽습니다.</p>
+      <br />
+      <p>벌써 5년 차가 되었음에도 매일 새로운 것을 배우며,</p>
+      <p>새로운 기술이 나오면 여전히 신기해합니다.</p>
+      <br />
+      <p>안 써봤던 기술을 사용하는 건 여전히 무섭지만</p>
+      <p>아직까지도 새로운 내용을 배워 업무에 적용하는 게 재밌습니다.</p>
+      <br />
+      <p>5년이나 했는데도 질리지 않는 걸 보면</p>
+      <p>직업을 잘 고른 것 같아요.</p>
+    </div>
   </AnimationSection>
 </template>
 
@@ -39,5 +41,10 @@ export default defineComponent({
 <style lang="scss" scoped>
 #about {
   color: var(--color-primary-text);
+  div {
+    text-align: center;
+    padding: 0 20px;
+    word-break: keep-all;
+  }
 }
 </style>
---
+++ src/views/pages/Landing/Career.vue
@@ -0,0 +1,121 @@
+<template>
+  <LandingSubPageLayout>
+    <template #title>Career Timeline</template>
+    <template #contents>
+      <div class="career-section-container">
+        <div id="line"></div>
+        <ul id="timeline-wrapper">
+          <li v-for="i in timeline_data">
+            <p class="career-title">{{ i.title }}</p>
+            <p class="job-title">{{ i.contents }}</p>
+            <p class="job-term">{{ i.datetime }}</p>
+          </li>
+        </ul>
+      </div>
+    </template>
+  </LandingSubPageLayout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import LandingSubPageLayout from "@/layouts/LandingSubPageLayout.vue"
+
+export default defineComponent({
+  components: {
+    LandingSubPageLayout,
+  },
+  data: function () {
+    return {
+      timeline_data: [
+        { title: "카카오스타일", contents: "프론트엔드 개발자", datetime: "2022. 07 -" },
+        { title: "모인 해외송금", contents: "프론트엔드 개발자", datetime: "2021. 02 - 2022. 06" },
+        { title: "에이아이비드", contents: "프론트엔드 개발자 (정규직)", datetime: "2019. 03 - 2020. 12" },
+        { title: "에이아이비드", contents: "프론트엔드 개발자 (프리랜서)", datetime: "2018. 06 - 2019. 03" },
+        { title: "안랩", contents: "QA/QC", datetime: "2017. 11 ~ 2018. 05" },
+        { title: "창성에이스산업", contents: "웹개발자", datetime: "2015. 11 ~ 2016. 02" },
+      ],
+    }
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.career-section-container {
+  position: relative;
+}
+
+#line {
+  position: absolute;
+  left: 50%;
+  right: 50%;
+  translate: -50% 0;
+  top: 0;
+  bottom: 0;
+  width: 2px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: var(--color-primary-text);
+  background: linear-gradient(
+    to bottom,
+    var(--background-color) 0%,
+    var(--color-primary-text) 10%,
+    var(--color-primary-text) 90%,
+    var(--background-color) 100%
+  );
+}
+
+#timeline-wrapper {
+  color: var(--color-primary-text);
+  display: grid;
+  grid-auto-flow: row;
+  row-gap: 20px;
+  max-width: $max-width;
+  margin: 0 auto;
+  grid-template-columns: 1fr;
+  /* border: 1px dashed black; */
+  padding: 60px 0;
+
+  li {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+  }
+
+  li:nth-child(odd) {
+    justify-self: flex-end;
+    margin-right: 50%;
+    text-align: right;
+    padding-right: 20px;
+  }
+
+  li:nth-child(even) {
+    justify-self: flex-start;
+    margin-left: 50%;
+    text-align: left;
+    padding-left: 20px;
+  }
+}
+
+.career-title {
+  font-size: 20px;
+  font-weight: 600;
+  @include mobile {
+    font-size: 18px;
+  }
+}
+.job-title {
+  font-size: 16px;
+  word-break: keep-all;
+  @include mobile {
+    font-size: 14px;
+  }
+}
+
+.job-term {
+  font-size: 14px;
+  color: var(--color-secondary-text);
+  @include mobile {
+    font-size: 14px;
+  }
+}
+</style>
