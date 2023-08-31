7317260b8e29d865627f6957b79a5b04f1766df7
TK-moon <tkmoon.dev@gmail.com>
2023-09-01 00:50:30 +0900

    :arrow_up: 디펜던시 버전업

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
+const { defineConfig } = require("@vue/cli-service")
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 const path = require("path")
 
-module.exports = {
+module.exports = defineConfig({
   configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
     devtool: "source-map",
     entry: "./src/main.ts",
     resolve: {
@@ -23,4 +26,4 @@ module.exports = {
   },
   publicPath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
   productionSourceMap: false,
-}
+})
