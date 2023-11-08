8fac359ba3c4ea75f92b9e844d8b3be8be93f003
TK-moon <tkmoon.dev@gmail.com>
2023-11-08 23:10:22 +0900

    :lipstick: 모달에 닫기 버튼 추가

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,12 @@ import store from "./store"
 import "@/styles/index.scss"
 
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faCog, faChevronLeft, faChevronRight, faPaperPlane } from "@fortawesome/free-solid-svg-icons"
+import { faCog, faChevronLeft, faChevronRight, faPaperPlane, faXmark } from "@fortawesome/free-solid-svg-icons"
 import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
 import VueSplide from "@splidejs/vue-splide"
 
-library.add(faCog, faChevronLeft, faChevronLeft, faChevronRight, faPaperPlane, faLinkedin, faGithubSquare)
+library.add(faCog, faChevronLeft, faChevronLeft, faChevronRight, faPaperPlane, faLinkedin, faGithubSquare, faXmark)
 
 createApp(App).use(store).use(router).use(VueSplide).component("font-awesome-icon", FontAwesomeIcon).mount("#app")
